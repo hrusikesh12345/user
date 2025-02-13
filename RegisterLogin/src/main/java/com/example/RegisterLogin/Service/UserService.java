07d0b41b71b3bb4ae661eaf1f86a6702930f2e7d
hrusikesh12345 <hrusikesh@spikewell.com>
2025-02-13 16:02:14 +0530

    Delete RegisterLogin directory

--- RegisterLogin/src/main/java/com/example/RegisterLogin/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.RegisterLogin.Service;
-import com.example.RegisterLogin.Dto.LoginDTO;
-import com.example.RegisterLogin.Dto.UserDTO;
-import com.example.RegisterLogin.payload.response.LoginMesage;
-public interface UserService {
-    String addUser(UserDTO userDTO);
-    LoginMesage loginUser(LoginDTO loginDTO);
-
-}
